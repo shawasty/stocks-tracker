41613971c35e2da882a501bfe228a80455e0d197
shawasty <shawasty@gmail.com>
2022-03-22 23:41:21 -0400

    added a table to project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import StockRow from './components/StockRow'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <table className="table mt-5">
+        <thead>
+          <tr>
+            <th>Ticker </th>
+            <th>price</th>
+            <th>Date</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+      <tbody>
+        <StockRow ticker ="aapl" />
+        <StockRow ticker ="goog" />
+        <StockRow ticker ="nsft" />
+        <StockRow ticker ="nsft" />
+      </tbody>
+      </table>
+    </div>
     </div>
   );
 }
---
+++ src/components/StockRow.jsx
@@ -0,0 +1,15 @@
+import React, { component } from 'react';
+
+
+const StockRow = () => {
+    return (
+       <tr>
+           <td>asdf</td>
+           <td>adafaf</td>
+           <td>afaga</td>
+           <td>afaga</td>
+       </tr>
+    );
+};
+
+export default StockRow;
